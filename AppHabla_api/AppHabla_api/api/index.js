dca9a8fcf431b7b28a6a8fae51a307188f47b9cf
UlisesXXXI <166647630+UlisesXXXI31@users.noreply.github.com>
2026-03-24 11:09:50 +0100

    Update index.js

--- AppHabla_api/AppHabla_api/api/index.js
+++ AppHabla_api/AppHabla_api/api/index.js
@@ -29,7 +29,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // --- Inicialización de Gemini ---
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 // --- Endpoints ---
 
