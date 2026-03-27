bd0d6b913bf116c34ffa2c77a71dfabfba6b4116
UlisesXXXI <166647630+UlisesXXXI31@users.noreply.github.com>
2026-03-27 06:07:49 +0100

    Update index.js

--- AppHabla_api/AppHabla_api/api/index.js
+++ AppHabla_api/AppHabla_api/api/index.js
@@ -30,7 +30,7 @@ const conectarDB = async () => {
 };
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
 
 // --- 2. RUTA DE INICIO (HEALTH CHECK) ---
 app.get('/', async (req, res) => {
